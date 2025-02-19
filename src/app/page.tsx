f37dc2795d594be4761cc559dc7f103f9380f95e
herob59 <hbinakeyi@gmail.com>
2025-02-19 19:39:11 +0100

    review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,15 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import ServiceSection from "@/components/ServiceSection";
-import Category from "@/sections/Category";
 import  './globals.css'
-import ProductSlider from "@/components/ProductSlider";
+
 
 export default function Home() {
   return (
     <main>
 
      <Header /> 
-       <HeroSection/>
-       <ProductSlider />     
-       <Category />
+       <HeroSection/>  
        <ServiceSection />
         <CounterSection /> 
         <ContactSection />
